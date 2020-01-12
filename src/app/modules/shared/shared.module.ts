c50ed9870da14d30ff3b3509d024753b538a6e1c
jaca982 <jacaszekhubert@gmail.com>
2020-01-13 00:57:34 +0100

    build error fix

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [DropdownComponent, CardComponent],
+  declarations: [ButtonComponent, DropdownComponent, CardComponent],
   imports: [CommonModule, RouterModule],
-  exports: [RouterModule, DropdownComponent, CardComponent]
+  exports: [RouterModule, ButtonComponent, DropdownComponent, CardComponent]
 })
 export class SharedModule {}
